45554753d7c079e434da9a8465e00a3fcb2b28e6
axiadotnet <57347435+axiadotnet@users.noreply.github.com>
2019-11-06 14:26:59 +0100

    FirstCommit

---
+++ FitnessManager/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using FitnessManager.Model;
+using System;
+
+namespace FitnessManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer C1 = new Customer("mario", "rossi", 20, Gender.Male);
+            C1.Age = -19;
+
+            Customer c2 = new Customer("GG01371347019", "Giorgio", "Gori", Gender.Male, DateTime.Now, 25, 1.82, 89);
+            Console.WriteLine(c2);
+        }
+    }
+}
